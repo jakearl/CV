b53b9834beae32ed7376edda05f6e238b222ba4b
Jake Earl <j.earl2325@outlook.com>
2025-01-02 12:37:45 -0500

    startup

---
+++ common_variables.R
@@ -0,0 +1,16 @@
+# Common non-csv data between CV and resume
+
+# Header with description of what is trying to be accomplished with this CV/Resume
+intro_text <- "Pharmacist (PharmD), and Health Economist (MS) with clinical and payor experience and in-depth training in Health Economics and Outcomes Research (HEOR). I have experience working for a Managed Care Organization where I developed and revised prior-authorization criteria and presented to the Pharmacy and Therapeutics (P&T) Committee to determine coverage. I have also built various economic models using Excel and I have conceptualized and conducted multiple Real-World Evidence (RWE) studies with claims data. My data analysis skills includes R, SAS, and SQL. I thoroughly enjoy health economics and strive to generate meaningful research that depicts a clear and impactful message. 
+
+I am currently searching for a position in HEOR that allows me to lead teams in utilizing innovative techniques to generate impactful RWE."
+
+
+# Language skills for skill bars visualization
+skills <- tribble(
+  ~skill,               ~level,
+  "R",                  4,
+  "SAS", 3,
+  "SQL",                2,
+  "VBA",             1.5
+)
